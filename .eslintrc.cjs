a9802b0f2bc18a890d4ffaf795876d5640868329
Mrlishizhen <718647063@qq.com>
2024-01-23 17:57:07 +0800

    测试提交

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,40 @@
 /* eslint-env node */
 
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:react-hooks/recommended',
-        'plugin:prettier/recommended', //配置prettier和eslint的适配问题
-        'prettier'
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended', //配置prettier和eslint的适配问题
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname,
+    extraFileExtensions: ['.less'],
+  },
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: true,
-        tsconfigRootDir: __dirname,
-        extraFileExtensions: ['.less'],
-    },
-    plugins: ['react-refresh', 'prettier'],
-    rules: {
-        'react-refresh/only-export-components': [
-            'warn',
-            { allowConstantExport: true },
-        ],
-        'prettier/prettier': [
-            'error',
-            {
-                endOfLine: 'auto',
-            },
-        ],
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off', //禁止成员访问类型 any 为 的值。
-        '@typescript-eslint/no-unsafe-return': 'off', //不允许从函数返回具有类型的 any 值。
-        '@typescript-eslint/no-floating-promises': 'off',
-        '@typescript-eslint/no-explicit-any': 'off', //禁止键入。 any
-        // "@typescript-eslint/no-unused-vars": "off"
-    },
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off', //禁止成员访问类型 any 为 的值。
+    '@typescript-eslint/no-unsafe-return': 'off', //不允许从函数返回具有类型的 any 值。
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-explicit-any': 'off', //禁止键入。 any
+    // "@typescript-eslint/no-unused-vars": "off"
+  },
 };
